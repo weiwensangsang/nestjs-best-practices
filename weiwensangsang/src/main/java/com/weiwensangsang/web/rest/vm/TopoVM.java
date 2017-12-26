8b5aadd9f208bc2be0ea2378acb9715c88878c0f
weiwensangsang <xalyf@bupt.edu.cn>
2017-12-26 16:29:28 +0800

    fix

--- weiwensangsang/src/main/java/com/weiwensangsang/web/rest/vm/TopoVM.java
+++ weiwensangsang/src/main/java/com/weiwensangsang/web/rest/vm/TopoVM.java
@@ -9,30 +9,9 @@ import java.util.stream.Collectors;
 
 public class TopoVM {
 
-    private List<Node> nodes;
-
-    private List<Link> links;
-
-
     private List<Location> locationList;
 
     private List<Path> paths;
-
-    public List<Node> getNodes() {
-        return nodes;
-    }
-
-    public void setNodes(List<Node> nodes) {
-        this.nodes = nodes;
-    }
-
-    public List<Link> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<Link> links) {
-        this.links = links;
-    }
 
     public List<Location> getLocationList() {
         return locationList;
@@ -62,17 +41,6 @@ public class TopoVM {
         TopoVM data = new TopoVM();
         data.setLocationList(locationList);
         data.setPaths(paths);
-        data.setNodes(
-            locationList.stream().map(location -> new Node(location.getPositionX())).collect(Collectors.toList())
-        );
-        data.setLinks(
-            paths.stream().map(path -> {
-                Link e = new Link();
-                e.source =  new Node(path.getFromWhere().getPositionX());
-                e.target =  new Node(path.getToWhere().getPositionX());
-                return e;
-            }).collect(Collectors.toList())
-        );
         return data;
     }
 }
