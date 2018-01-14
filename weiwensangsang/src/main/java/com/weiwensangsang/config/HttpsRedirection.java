2043a132b06fcf114abd3be490d92c8a9564d19d
weiwensangsang <xalyf@bupt.edu.cn>
2018-01-14 15:37:31 +0800

    fix

---
+++ weiwensangsang/src/main/java/com/weiwensangsang/config/HttpsRedirection.java
@@ -0,0 +1,47 @@
+package com.weiwensangsang.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!dev")
+@Configuration
+public class HttpsRedirection {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        try {
+            tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        } catch (Exception e) {
+
+        }
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() throws Exception{
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+
+        return connector;
+    }
+}
