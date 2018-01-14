f5b71b6f47829ff6f199d44516814c8f7f2a8ca9
weiwensangsang <xalyf@bupt.edu.cn>
2018-01-14 13:21:21 +0800

    '

--- weiwensangsang/src/main/webapp/app/face-check/face-check.controller.js
+++ weiwensangsang/src/main/webapp/app/face-check/face-check.controller.js
@@ -19,6 +19,38 @@
         vm.textLength = 0;
         vm.showLucky = 'normal';
 
+        var video = document.getElementById('video'),
+            canvas = document.getElementById('canvas'),
+            snap = document.getElementById('tack'),
+            img = document.getElementById('img'),
+            vendorUrl = window.URL || window.webkitURL;
+
+        //媒体对象
+        navigator.getMedia = navigator.getUserMedia ||
+            navagator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
+        navigator.getMedia({
+            video: true, //使用摄像头对象
+            audio: false  //不适用音频
+        }, function(strem){
+            console.log(strem);
+            video.src = vendorUrl.createObjectURL(strem);
+            video.play();
+        }, function(error) {
+            //error.code
+            console.log(error);
+        });
+        snap.addEventListener('click', function(){
+
+            //绘制canvas图形
+            canvas.getContext('2d').drawImage(video, 0, 0, 400, 300);
+
+            //把canvas图像转为img图片
+            img.src = canvas.toDataURL("image/png");
+
+        })
+
         function changeModel() {
             var data = vm.config;
             if (vm.model === 'light') {
