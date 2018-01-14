9c33485e46b13fd97cbfdd511ed743dcc85572e8
weiwensangsang <xalyf@bupt.edu.cn>
2018-01-14 15:25:40 +0800

    fix

--- weiwensangsang/src/main/java/com/weiwensangsang/config/SecurityConfiguration.java
+++ weiwensangsang/src/main/java/com/weiwensangsang/config/SecurityConfiguration.java
@@ -6,8 +6,10 @@ import com.weiwensangsang.security.jwt.*;
 import io.github.jhipster.security.*;
 
 import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -37,6 +39,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final TokenProvider tokenProvider;
 
     private final CorsFilter corsFilter;
+
+    @Autowired
+    private Environment env;
 
     public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService,
             TokenProvider tokenProvider,
@@ -84,6 +89,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        if (env.acceptsProfiles("!dev"))http.requiresChannel().anyRequest().requiresSecure();
         http
             .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling()
